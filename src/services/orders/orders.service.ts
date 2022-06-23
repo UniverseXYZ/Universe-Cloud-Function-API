82864a3a23497843561b009c5149dc9431ab413d
Paul Donskikh <donskikhinvest@gmail.com>
2022-06-23 09:00:01 -0400

    sc-5001: Look through the CF filters to add support for bundles

--- src/services/orders/orders.service.ts
+++ src/services/orders/orders.service.ts
@@ -11,56 +11,57 @@ import { Utils } from '../../utils';
 export const getBundleOrdersByTokens = async (tokens) => {
   let value = [];
   
-  if (tokens.length) {
-    const utcTimestamp = Utils.getUtcTimestamp();
-
-    const contractsTokensMap = {};
-    tokens.forEach((token) => {
-      if (contractsTokensMap.hasOwnProperty(token.contractAddress.toLowerCase())) {
-        contractsTokensMap[token.contractAddress.toLowerCase()].push(token.tokenId);
-      } else {
-        contractsTokensMap[token.contractAddress.toLowerCase()] = [token.tokenId];
-      }
-    });
-
-    const bundleOrders = await OrderModel.find({
-      $and: [
-        { side: OrderSide.SELL },
-        { status: OrderStatus.CREATED },
-        { 'make.assetType.assetClass': AssetClass.ERC721_BUNDLE },
-        { $or: [{ start: { $lt: utcTimestamp } }, { start: 0 }] },
-        { $or: [{ end: { $gt: utcTimestamp } }, { end: 0 }] },
-        {
-          'make.assetType.contracts': {
-            $in: Object.keys(contractsTokensMap),
-          }
-        },
-      ],
-    });
-
-    bundleOrders.forEach((bundleOrder) => {
-      // "contracts" and "tokenIds" magically disapear from bundleOrder.make.assetType!
-      // I cannot explain that, try it yourself:
-      // console.log(bundleOrder.make.assetType)
-      // console.log(bundleOrder.make.assetType.contracts)
-      // console.log(bundleOrder.make.assetType.tokenIds)
-      bundleOrder = JSON.parse(JSON.stringify(bundleOrder));
-          
-      for (let i = 0; i < bundleOrder.make.assetType.contracts.length; i++) {
-        if (
-          contractsTokensMap.hasOwnProperty(bundleOrder.make.assetType.contracts[i]) &&
-          Utils.findArrayIntersection(
-            contractsTokensMap[bundleOrder.make.assetType.contracts[i]],
-            bundleOrder.make.assetType.tokenIds[i],
-          ).length
-        ) {
-          value.push(bundleOrder);
-          break;
-        }
-      }
-    });
-  
+  if (!tokens.length) {
+    return [];
   }
+  
+  const utcTimestamp = Utils.getUtcTimestamp();
+
+  const contractsTokensMap = {};
+  tokens.forEach((token) => {
+    if (contractsTokensMap.hasOwnProperty(token.contractAddress.toLowerCase())) {
+      contractsTokensMap[token.contractAddress.toLowerCase()].push(token.tokenId);
+    } else {
+      contractsTokensMap[token.contractAddress.toLowerCase()] = [token.tokenId];
+    }
+  });
+
+  const bundleOrders = await OrderModel.find({
+    $and: [
+      { side: OrderSide.SELL },
+      { status: OrderStatus.CREATED },
+      { 'make.assetType.assetClass': AssetClass.ERC721_BUNDLE },
+      { $or: [{ start: { $lt: utcTimestamp } }, { start: 0 }] },
+      { $or: [{ end: { $gt: utcTimestamp } }, { end: 0 }] },
+      {
+        'make.assetType.contracts': {
+          $in: Object.keys(contractsTokensMap),
+        }
+      },
+    ],
+  });
+
+  bundleOrders.forEach((bundleOrder) => {
+    // "contracts" and "tokenIds" magically disapear from bundleOrder.make.assetType!
+    // I cannot explain that, try it yourself:
+    // console.log(bundleOrder.make.assetType)
+    // console.log(bundleOrder.make.assetType.contracts)
+    // console.log(bundleOrder.make.assetType.tokenIds)
+    bundleOrder = JSON.parse(JSON.stringify(bundleOrder));
+        
+    for (let i = 0; i < bundleOrder.make.assetType.contracts.length; i++) {
+      if (
+        contractsTokensMap.hasOwnProperty(bundleOrder.make.assetType.contracts[i]) &&
+        Utils.findArrayIntersection(
+          contractsTokensMap[bundleOrder.make.assetType.contracts[i]],
+          bundleOrder.make.assetType.tokenIds[i],
+        ).length
+      ) {
+        value.push(bundleOrder);
+        break;
+      }
+    }
+  });
 
   return value;
 }
