88f905170c9893da85f1d002e28c8d1fe51ebb8a
Coiling-Dragon <66465846+Coiling-Dragon@users.noreply.github.com>
2022-07-18 16:41:36 +0300

    Fixed bugs in minPrice and maxPrice parameters

--- src/errors/positive-number-validation-error.ts
+++ src/errors/positive-number-validation-error.ts
@@ -1,13 +1,26 @@
 import { BaseError } from './base-error';
 import { ERROR_MESSAGES, HTTP_STATUS_CODES } from './error-messages';
 
-export class PositiveNumberValidationError extends BaseError {
+export class PositiveIntValidationError extends BaseError {
   propertyName: string;
 
   constructor(parameterName: string) {
     super(
       HTTP_STATUS_CODES.BAD_REQUEST,
       ERROR_MESSAGES.POSTIVE_INTEGER(parameterName),
+    );
+
+    this.propertyName = parameterName;
+  }
+}
+
+export class PositiveNumberValidationError extends BaseError {
+  propertyName: string;
+
+  constructor(parameterName: string) {
+    super(
+      HTTP_STATUS_CODES.BAD_REQUEST,
+      ERROR_MESSAGES.POSTIVE_NUMBER(parameterName),
     );
 
     this.propertyName = parameterName;
