136cf42d81d4531585dadeebf7ffe07f10807986
vikinatora <vikinatora@gmail.com>
2022-07-25 15:02:16 +0300

    improve deploy file

--- gcf.sh
+++ gcf.sh
@@ -15,7 +15,10 @@ PROJ=polymorphmetadata
 # This region seems to work well for me, all the others are presumably about the same.
 REGION=us-central1
 
-FNAME=nfts
-gcloud beta functions deploy $FNAME --project $PROJ --trigger-http  --runtime nodejs16 --allow-unauthenticated --update-env-vars ETHEREUM_CHAIN_ID=4,NODE_ENV=production,DB_URL=mongodb+srv://universe-db-account:y7rYKn6Bw41PbB3E@universe-dev-cluster-at.lrbek.mongodb.net/dev-universe-datascraper?retryWrites=true&w=majority&ssl=true
+#FNAME=nfts
+#gcloud beta functions deploy $FNAME --project $PROJ --entry-point=nfts --trigger-http  --runtime nodejs16 --allow-unauthenticated --update-env-vars ETHEREUM_CHAIN_ID=4,NODE_ENV=production,DB_URL=mongodb+srv://universe-db-account:y7rYKn6Bw41PbB3E@universe-dev-cluster-at.lrbek.mongodb.net/dev-universe-datascraper?retryWrites=true&w=majority&ssl=true
+
+FNAME=nfts-prod
+gcloud beta functions deploy $FNAME --project $PROJ --entry-point=nfts --trigger-http  --runtime nodejs16 --allow-unauthenticated --update-env-vars ETHEREUM_CHAIN_ID=1,NODE_ENV=production,DB_URL=mongodb+srv://universe-prod-account:LEvRA2owmFi3791Ox6Xx@universe-prod-cluster-a.5brb2bu.mongodb.net/prod-universe-datascraper?retryWrites=true&w=majority
 
 # Upon success, it will print the URL.
