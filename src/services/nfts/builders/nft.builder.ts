192a47544025faeace053330e036c849f1aeacc1
Coiling-Dragon <66465846+Coiling-Dragon@users.noreply.github.com>
2022-10-04 15:36:02 +0300

    save tokenIds array sorted sequence

--- src/services/nfts/builders/nft.builder.ts
+++ src/services/nfts/builders/nft.builder.ts
@@ -102,6 +102,21 @@ export const buildNftQueryFilters = async (
       break;
   }
 
+
+  if(tokenIds){
+    const tokenIdsSplit = tokenIds.replace(/\s/g, '').split(',');
+
+    // create a new field that will be used in the sorting by __id_posn
+    nftFilters.push({
+      "$addFields" : { "__id_posn" : { "$indexOfArray" : [ tokenIdsSplit, "$tokenId" ] } } 
+    })
+
+    // we need to delete the other sorting options and add a new one that sorts by array position
+    delete sort.tokenId
+    delete sort.updatedAt
+    sort.__id_posn = 1
+  }
+
   console.log('NFT FILTERS:');
   console.log(nftFilters);
 
